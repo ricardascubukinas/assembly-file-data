f46dcd128d5266b463e9545d9c371bedc835bcbe
spidax <spidax1881@gmail.com>
2020-11-12 16:35:41 +0200

    first commit

---
+++ U2.asm
@@ -0,0 +1,25 @@
+;; Author Ričardas Čubukinas
+;;
+
+.model small
+.stack 100h
+
+JUMPS
+
+
+.data
+    help        db 'This program prints all symbol differences & their positions between two files', 13,10
+    error_file  db 'File couldn't be opened for reading'
+.code
+    
+start:
+    mov ax, @data
+    mov ds, ax
+
+
+ending:
+    mov ax, 4Ch
+    mov al, 00h
+    int 21h
+            
+end start
