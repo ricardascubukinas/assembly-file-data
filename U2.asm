f0ec292f4d70abe387a13dd657ff0e898bd1ba43
spidax <spidax1881@gmail.com>
2020-11-26 22:07:59 +0200

    Full comments and man page

--- U2.asm
+++ U2.asm
@@ -13,15 +13,22 @@ JUMPS
 
 
 .data
-    error_file_destination  db "File couldn't be opened for writing", 13, 10, '$'
-    error_file_source       db "File couldn't be opened for reading", 13, 10, '$'
+    error_file_destination  db "File couldn't be opened for writing", 13, 10, "$"
+    error_file_source       db "File couldn't be opened for reading", 13, 10, "$"
     error_empty_file        db "The file: $"
     error_emtpy_file2       db ", was empty", 13, 10, "$"
-    msg_help                db "NAME",13,10,9, "U2 - Second assigment 1st variant", 13,10,13,10, '$'
-    msg_help2               db "SYNOPSIS",13,10,9, "U2 [/?] FILENAME [FILENAME2]...",13,10,13,10, '$'
-    msg_help3               db "DESCRIPTION",13,10,9, "This program counts the number of symbols, uppercase letters",13,10, '$'
-    msg_help4               db 9, "lowercase letters as well as words of multiple files",13,10,13,10, '$'
-    msg_help5               db "EXIT STATUS", '$'
+    msg_help                db "NAME",13,10,9, "U2 - Second assigment 1st variant", 13,10,13,10, "$"
+    msg_help2               db "SYNOPSIS",13,10,9, "U2 [/?] FILENAME [FILENAME2]...",13,10,13,10, "$"
+    msg_help3               db "DESCRIPTION",13,10,9, "This program counts the number of symbols, uppercase letters",13,10, "$"
+    msg_help4               db 9, "lowercase letters as well as words of multiple files",13,10,13,10, "$"
+    msg_help5               db "EXIT STATUS", 13,10,"$"
+    msg_help6               db 9, "U2 exits with 0 if the files were opened/wrote correctly",13,10, "$" 
+    msg_help7               db 9, "status 1 if it failed to open any files or write the results",13,10,13,10, "$"
+    msg_help8               db "OPTIONS",13,10, 9, "/?", 13,10,9,9,"Prints this page",13,10,13,10, "$"
+    msg_help9               db "EXAMPLES", 13,10,9, "To show statistical data of 2 files use", 13,10, 9, 9, "$"
+    msg_help10              db "U2 'example.txt' 'example2.txt'", 13,10,13,10, "$"
+    msg_help11              db "NOTES",13,10,9, "Maximum filename length is 8 without ext.", "$"
+
     msg_symbol_count        db "Symbols in file: $"
     msg_uppercase_count     db "Uppercase letters in file: $"
     msg_lowercase_count     db "Lowercase letters in file: $"
@@ -123,12 +130,14 @@ open_file_read:
     mov source_file_handle, ax
 
 read_data:
+    ;; use a buffer, size 20
     mov bx, source_file_handle
     mov dx, offset read_buffer
     mov cx, CONST_READ_BUFFER_SIZE
     mov ah, 3Fh
     int 21h
 
+    ;; Is the buffer empty?
     mov cx, ax
     cmp ax, 0
     jne set_options
@@ -141,16 +150,18 @@ read_data:
 
 
 set_options:
+    ;; Prepare for reading and writing
     mov si, offset read_buffer
     mov bx, dest_file_handle
 
-
+    ;; If the buffer reached is end or empty close the file
     cmp source_file_handle, 0
     jne save_count
     cmp byte ptr ds:[si], 13
     je close_file
 
 save_count:
+    ;; Counting symbols in buffer
     call    check_buffer_count
     mov dx, symbol_count
     add dx, ax
@@ -159,21 +170,27 @@ save_count:
     mov cx, ax
 
 buffer_loop:
+    ;; Load byte at address DS:(E)SI into AL
     lodsb
 
+    ;; Checking if it is just a symbol
+    ;; an uppercase letter, lowercase or space
     mov bl, al
     call check_symbol 
 
+    ;; loop through the buffer
     loop buffer_loop
     jmp check_buffer
 
 check_buffer:
+    ;; If the file is fully read, write the results
     pop ax
     cmp ax, CONST_READ_BUFFER_SIZE
     je read_data
     jmp write_result
 
 write_result:
+    ;; First the filename
     mov ah, 40h
     mov bx, dest_file_handle
     mov cx, filename_length
@@ -182,6 +199,7 @@ write_result:
 
     call    print_newline
 
+    ;; The amount of symbols
     mov ah, 40h
     mov bx, dest_file_handle
     mov cx, 17
@@ -192,6 +210,7 @@ write_result:
     call    convert_print
     call    print_newline
 
+    ;; The amount of lowercase letters
     mov ah, 40h
     mov bx, dest_file_handle
     mov cx, 27
@@ -202,6 +221,7 @@ write_result:
     call    convert_print
     call    print_newline
 
+    ;; The amount of uppercase letters
     mov ah, 40h
     mov bx, dest_file_handle
     mov cx, 27
@@ -212,6 +232,7 @@ write_result:
     call    convert_print
     call    print_newline
 
+    ;; The amount of words
     mov ah, 40h
     mov bx, dest_file_handle
     mov cx, 15
@@ -223,11 +244,14 @@ write_result:
     call    print_newline
     call    print_newline
 
+    ;; Reset the variables used for counting symbols, etc.
     call reset_variables
 
+    ;; Keep reading source files
     jmp read_source_file
 
 help:
+    ;; A man page is nice
     mov ax, @data
     mov ds, ax
 
@@ -247,10 +271,40 @@ help:
     mov ah, 09h
     int 21h
 
+    mov dx, offset msg_help5
+    mov ah, 09h
+    int 21h
+
+    mov dx, offset msg_help6
+    mov ah, 09h
+    int 21h
+
+    mov dx, offset msg_help7
+    mov ah, 09h
+    int 21h
+
+    mov dx, offset msg_help8
+    mov ah, 09h
+    int 21h
+
+    mov dx, offset msg_help9
+    mov ah, 09h
+    int 21h
+    
+    mov dx, offset msg_help10
+    mov ah, 09h
+    int 21h
+    
+    mov dx, offset msg_help11
+    mov ah, 09h
+    int 21h
+    
+
     jmp ending
 
 
 error_destination:
+    ;; Why u can't open the destination file?
     mov ax, @data
     mov ds, ax
 
@@ -262,9 +316,11 @@ error_destination:
     int 21h
 
     mov ax, 4C02h
+    mov al, 1
     int 21h
 
 error_source:
+    ;; Why u can't open the source file?
     mov ax, @data
     mov ds, ax
 
@@ -276,20 +332,24 @@ error_source:
     int 21h
 
     mov ax, 4C01h
+    mov al, 1
     int 21h
 
 close_file:
+    ;; File'us deletus
     pop ds si
     mov ah, 3Eh
     mov bx, dest_file_handle
     int 21h
 
 ending:
+    ;; Adios
     mov ax, 4C00h
     mov al, 00h
     int 21h
 
 skip_spaces PROC near
+    ;; Who writes more than 1 space honestly?
     skip_spaces_iterate:
         cmp byte ptr ds:[si], ' '
         jne skip_spaces_end
@@ -306,18 +366,21 @@ read_filename PROC near
 
 	call	skip_spaces
 read_filename_start:
+    ;; How many files can I read
  	cmp	byte ptr ds:[si], 13
     je	read_filename_end
     cmp	byte ptr ds:[si], ' '
     jne	read_filename_next
 
 read_filename_end:
+    ;; Too many to count
    	mov	al, '$'
    	stosb
    	pop	ax
    	ret
 
 read_filename_next:
+    ;; Dirty tricks
     push ax ds
     mov ax, @data
     mov ds, ax
@@ -331,6 +394,7 @@ read_filename_next:
 read_filename ENDP
 
 check_symbol PROC near
+    ;; Gotta find out in which category does the symbol belong if any
     push ax
     
     is_lowercase:
@@ -367,7 +431,7 @@ check_symbol ENDP
 
 
 convert_print PROC near
-    
+   ;; We convert the hexadecimal to decimal and print it
    xor cx,cx  
    convert: 
         sub dx, dx
@@ -407,6 +471,7 @@ print_newline ENDP
 
 
 reset_variables PROC near
+    ;; No leftover cumulative counting anymore
     mov symbol_count, 0
     mov lowercase_count, 0
     mov uppercase_count, 0
@@ -418,6 +483,9 @@ reset_variables PROC near
 reset_variables ENDP
 
 check_buffer_count PROC near
+    ;; If it is the first buffer and it's not empty
+    ;; Then means there's at least 1 word in it even
+    ;; if there are no spaces in that file ;)
     cmp buffer_count, 0
     jne check_buffer_count_end
     inc word_count
@@ -429,6 +497,7 @@ check_buffer_count PROC near
 check_buffer_count ENDP
 
 empty_file PROC near
+    ;; We don't need statistical data of empty files
     mov ah, 40h
     mov bx, dest_file_handle
     lea dx, error_empty_file
